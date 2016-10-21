2799e5eac3d4acbc65471da62c799c27a908537b
Cyril B <cyril.brouillard@gmail.com>
2016-10-21 10:14:09 +0200

    Compression de la photo sur ajout wish

--- epsilon/src/main/java/com/headbangers/epsilon/v3/activity/wish/AddWishActivity.java
+++ epsilon/src/main/java/com/headbangers/epsilon/v3/activity/wish/AddWishActivity.java
@@ -1,6 +1,8 @@
 package com.headbangers.epsilon.v3.activity.wish;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -34,7 +36,11 @@ import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -133,19 +139,31 @@ public class AddWishActivity extends AbstractEpsilonActivity implements Refresha
 
     @OnActivityResult(REQUEST_IMAGE_CAPTURE)
     void photoTaken() {
-        photo.setImageURI(Uri.parse(photoPath));
-        photo.setVisibility(View.VISIBLE);
+        if (photoPath != null) {
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(new File(URI.create(photoPath))));
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 600, 600, true);
+                scaledBitmap.compress(Bitmap.CompressFormat.PNG, 0, new FileOutputStream(new File(URI.create(photoPath))));
+
+                photo.setImageURI(Uri.parse(photoPath));
+                photo.setVisibility(View.VISIBLE);
+
+            } catch (FileNotFoundException e) {
+                this.photoPath = null;
+                photo.setVisibility(View.GONE);
+            }
+
+        } else {
+            photo.setVisibility(View.GONE);
+        }
     }
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "WISH_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
+
+        File image = new File(storageDir, imageFileName + ".jpg");
 
         photoPath = "file:" + image.getAbsolutePath();
         return image;
